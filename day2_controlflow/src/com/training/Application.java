e01368c504f21216eb0643de332011c0c99c8f41
1-n-only-nobody <16bce100@nirmauni.ac.in>
2020-09-16 11:05:44 +0530

    day2 Assignment commit 1

---
+++ day2_controlflow/src/com/training/Application.java
@@ -0,0 +1,21 @@
+package com.training;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Product tv = new Product(1010, "LED TV", 23000.00, 3);
+		
+		ManageProduct mgr = new ManageProduct();
+		
+		double discount = mgr.calculateDiscount(tv);
+		
+		System.out.println("Discount :--> " + discount);
+		
+		double tax = mgr.calculateTax(tv);
+		System.out.println("Tax Percentage :--> " + (tax * 100) + "%");
+//		mgr.showProducts();
+	}
+	
+}
---
+++ day2_controlflow/src/com/training/ManageProduct.java
@@ -0,0 +1,52 @@
+package com.training;
+
+public class ManageProduct {
+	
+	//Example for If-Else
+	public double calculateDiscount(Product product) {
+		
+		double discount = 0.0;
+		
+		if (product.getRatePerUnit() > 500 && product.getRatePerUnit() <= 1000) {
+			
+			discount = 0.10;
+		}
+		else if (product.getRatePerUnit() > 1000) {
+			
+			discount = 0.15;
+		}
+		return discount;
+	}
+	
+	public double calculateTax(Product product) {
+		
+		double tax = 0.0;
+		
+		switch (product.getProductType()) {
+		
+		case 1 : 
+			tax = 0.05;
+			break;
+		
+		case 2 :
+			tax = 0.10;
+			break;
+			
+		case 3 :
+			tax = 0.15;
+			break;
+			
+		default :
+			tax = 0.28;
+			break;
+		}
+		return tax;
+	}
+	public void showProducts() {
+		
+		for (int i = 0; i < 10; i++) {
+			
+			System.out.println("product");
+		}
+	}
+}

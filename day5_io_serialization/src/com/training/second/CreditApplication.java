d26ecf11991e60a07bf85cf8ade3a2445578753c
1-n-only-nobody <16bce100@nirmauni.ac.in>
2020-09-21 16:06:55 +0530

    I don't know

--- day5_io_serialization/src/com/training/second/CreditApplication.java
+++ day5_io_serialization/src/com/training/second/CreditApplication.java
@@ -1,10 +1,38 @@
 package com.training.second;
 
+import java.io.File;
+
+import com.training.model.CreditCard;
+import com.training.services.BusinessService;
+
 public class CreditApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		CreditCard creditCard[] = new CreditCard[3];
+		CreditCard newcreditCard[] = new CreditCard[3];
+		
+		CreditCard creditCard1 = new CreditCard("Abcd", 123456, 5000.00, 145);
+		CreditCard creditCard2 = new CreditCard("Bcde", 234567, 6000.00, 458);
+		CreditCard creditCard3 = new CreditCard("Cdef", 345678, 7000.00, 582);
+		creditCard[0] = creditCard1;
+		creditCard[1] = creditCard2;
+		creditCard[2] = creditCard3;
+		
+		BusinessService service = new BusinessService();
+		
+		System.out.println("Enter file name: ");
+		File file = new File("abc.txt");
+//		System.out.println(file.toString());
+		System.out.println("Enter path to file : ");
+		String path = "D:/AddToROG/";
+//		service.storeInCharacters(creditCard, file);
+//		service.storeInBytes(creditCard, file);
+//		service.deleteFile(file);
+		newcreditCard = service.readFile(file, 1);
+		System.out.println(newcreditCard[1]);
+		
 	}
 
 }
--- day5_io_serialization/src/com/training/services/BusinessService.java
+++ day5_io_serialization/src/com/training/services/BusinessService.java
@@ -1,5 +1,202 @@
 package com.training.services;
 
-public class BusinessService {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 
+import com.training.model.Book;
+import com.training.model.CreditCard;
+
+public class BusinessService {
+	
+	public boolean storeInCharacters(CreditCard[] creditCard, File file) {
+		
+		PrintWriter outChar = null;
+		String s = null;
+		try {
+			outChar = new PrintWriter(new FileWriter(file, true));
+			
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		for (CreditCard eachCreditCard : creditCard) {
+		s =  eachCreditCard.toString();
+		outChar.println(s);
+		}
+		outChar.close();
+		return true;
+		
+		
+	}
+
+	public boolean storeInCharacters(CreditCard[] creditCard, File file, String path) {
+		
+		PrintWriter outChar = null;
+		String tempPath = file.toString();
+		String s = null;
+		try {
+			outChar = new PrintWriter(new FileWriter(path + "/" + tempPath, true));
+			
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		for (CreditCard eachCreditCard : creditCard) {
+		s =  eachCreditCard.toString();
+		outChar.println(s);
+		}
+		outChar.close();
+		return true;
+		
+		
+	}
+
+	public boolean storeInBytes(CreditCard[] creditCard, File file) {
+		
+		ObjectOutputStream outStream = null;
+		try {
+			outStream = new ObjectOutputStream(new FileOutputStream(file));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		try {
+			
+			outStream.writeObject(creditCard);
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+	public boolean storeInBytes(CreditCard[] creditCard, File file, String path) {
+		
+		ObjectOutputStream outStream = null;
+		String tempPath = file.toString();
+		try {
+			outStream = new ObjectOutputStream(new FileOutputStream(path + "/" + tempPath, false));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		try {
+			
+			outStream.writeObject(creditCard);
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+	public CreditCard[] readFile(File file, int choice) {
+		
+		CreditCard[] creditCards = new CreditCard[3];
+		CreditCard creditCard = null;
+		int i = 0;
+		if(choice == 0) {
+
+
+//		
+		ObjectInputStream inStream = null;
+		
+		try {
+			inStream = new ObjectInputStream(new FileInputStream(file));
+			creditCards = (CreditCard[]) inStream.readObject();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+		else {
+		BufferedReader in = null;
+		String line = null;
+		String attributes[] = new String[4];
+		try {
+			in = new BufferedReader(new FileReader(file));
+			
+			while ((line = in.readLine()) != null) {
+				attributes = line.split(",");
+				System.out.println(attributes[0] + attributes[1] + attributes[2] + attributes[3]);
+				creditCard = new CreditCard(attributes[0], Long.parseLong(attributes[1].trim()), Double.parseDouble(attributes[2].trim()), Integer.parseInt(attributes[3].trim()));
+				creditCards[i] = creditCard;
+				i++;
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		}
+		return creditCards;
+//		return null;
+	
+	}
+	
+	public void deleteFile(File file, long cardNumber) {
+		
+		try {
+			FileWriter outChar = new FileWriter(file,true);
+			
+			outChar.write("");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteFile(File file, String path) {
+		
+		String tempPath = file.toString();
+		try {
+			FileWriter outChar = new FileWriter(path + "/" + tempPath);
+			
+			outChar.write("");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }

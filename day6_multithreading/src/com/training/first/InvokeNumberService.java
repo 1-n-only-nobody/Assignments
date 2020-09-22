7e57d418a15e03fe139fe574aaee0c1b4f91f31f
1-n-only-nobody <16bce100@nirmauni.ac.in>
2020-09-22 11:06:01 +0530

    constructor added in InvokeNumberService

--- day6_multithreading/src/com/training/first/InvokeNumberService.java
+++ day6_multithreading/src/com/training/first/InvokeNumberService.java
@@ -2,6 +2,16 @@ package com.training.first;
 
 public class InvokeNumberService implements Runnable {
 
+	private int countTo;
+	private String threadName;
+	
+	public InvokeNumberService(int countTo, String threadName) {
+		super();
+		this.countTo = countTo;
+		this.threadName = threadName;
+	}
+
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub

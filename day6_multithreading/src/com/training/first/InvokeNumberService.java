2b87cc147d80065785b5ef13666779d00be127ed
1-n-only-nobody <16bce100@nirmauni.ac.in>
2020-09-22 11:17:10 +0530

    Updated InvokeNumberService

--- day6_multithreading/src/com/training/first/InvokeNumberService.java
+++ day6_multithreading/src/com/training/first/InvokeNumberService.java
@@ -1,21 +1,37 @@
 package com.training.first;
 
+import com.training.services.NumberService;
+
 public class InvokeNumberService implements Runnable {
 
 	private int countTo;
 	private String threadName;
+	private NumberService service;
 	
 	public InvokeNumberService(int countTo, String threadName) {
 		super();
+		
+		//Setting up parameters passed from the main method
 		this.countTo = countTo;
 		this.threadName = threadName;
+		
+		//Creating an instance of our service
+		this.service = new NumberService();
+		
+		//Passing the service instance, which is a runnable, to thread class along with a name
+		Thread thread = new Thread(this, threadName);
+		
+		//Calling the start() method, makes the thread eligible for running
+		thread.start();
 	}
-
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 
+		//Calling the code that has to be executed in the thread
+		//Indirectly adding code to the run method, by calling another method
+		this.service.sumNumbers(countTo);
 	}
 
 }

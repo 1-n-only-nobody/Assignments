715975e04e1de6274a636de05ec3ad7689a81e9f
1-n-only-nobody <16bce100@nirmauni.ac.in>
2020-09-22 12:37:16 +0530

    Join Example updated

--- day6_multithreading/src/com/training/firstExtended/ThreadJoinExample.java
+++ day6_multithreading/src/com/training/firstExtended/ThreadJoinExample.java
@@ -1,13 +1,51 @@
 package com.training.firstExtended;
 
+import java.io.IOException;
+
 public class ThreadJoinExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Main Thread Starts.");
+		
 		Thread t = new Thread() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+//				super.run();
+				
+				System.out.println("Child Thread Starts.");
+				
+				try {
+					
+					System.out.println("Enter a key.");
+					
+					int key = System.in.read();
+					
+					System.out.println((char) key);
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				System.out.println("Child Thread Ends.");
+			}
 			
 		};
+		
+		t.start();
+		
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Main Thread Ends.");
 	}
 
 }

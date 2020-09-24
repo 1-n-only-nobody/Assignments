65decbebc590dc5648a0de92a7d7b606adf535f5
1-n-only-nobody <16bce100@nirmauni.ac.in>
2020-09-24 12:51:17 +0530

    much change commit

---
+++ day8_jdbc/src/com/training/Application.java
@@ -0,0 +1,14 @@
+package com.training;
+
+import com.training.utils.ConnectionUtility;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(ConnectionUtility.getDerbyConnection());
+		
+	}
+
+}
